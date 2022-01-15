ebc3d173e720c08e2b1fd042694eeeebe5c49233
surganovahome2021 <97347027+surganovahome2021@users.noreply.github.com>
2022-01-15 14:02:49 +0300

    add Task1 Deposit

--- Tasks/Kvaskov/01112022_discriminant.py
+++ Tasks/Kvaskov/01112022_discriminant.py
@@ -5,7 +5,12 @@ b = float(input ("b = "))
 c = float(input ("c = "))
 d = b ** 2-4 * a * c
 print("discriminant = ",d)
-x1 = (-b+math.sqrt (d))/(2 * a)
-x2 = (-b-math.sqrt (d))/(2 * a)
-print("X1 = ",x1)
-print("X2 = ",x2)
+if d>0:
+    x1 = (-b+math.sqrt (d))/(2 * a)
+    x2 = (-b-math.sqrt (d))/(2 * a)
+    print("X1 = ",x1)
+    print("X2 = ",x2)
+elif d == 0:
+    x1 = -(b/2*a)
+    print("x= ", x1)
+else: print("no roots")    
