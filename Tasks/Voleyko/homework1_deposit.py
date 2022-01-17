f07983a2a5deefd3e042bd3e66810f8aadc274ca
VoleykoN <nadiavoleyko@gmail.com>
2022-01-17 18:03:22 +0300

    added some changes to the taskHW

--- Tasks/Voleyko/homework1_deposit.py
+++ Tasks/Voleyko/homework1_deposit.py
@@ -1,7 +1,12 @@
-d_amount=20000
-term=5
-rate=15
+import decimal
+from decimal import Decimal
+
+d_amount=decimal.Decimal('20000')
+term=decimal.Decimal('5')
+rate=decimal.Decimal('15')
 
 deposit=d_amount*((1+rate/100/12)**(term*12))
 
-print('Cумма на счету в конце составит:',int(deposit),'',end='BYN')
+
+print('Сумма на счету в конце составит:', deposit.quantize(Decimal('1.00')),'', end='BYN')
+#print('Cумма на счету в конце составит:',(deposit.quantize(1.00)),'',end='BYN')
