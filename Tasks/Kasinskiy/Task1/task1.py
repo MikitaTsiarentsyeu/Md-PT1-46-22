719ef7313f284bfc07a5c44078cb33cf288f2a39
masterkas <mastervolkas@gmail.com>
2022-01-17 19:40:56 +0300

    Update task1.py

--- Tasks/Kasinskiy/Task1/task1.py
+++ Tasks/Kasinskiy/Task1/task1.py
@@ -4,6 +4,5 @@ initial_deposit = 20000
 interest_rate = 15
 deposit_term = 60
 
-final_income = d.Decimal(initial_deposit*(1+interest_rate/100/12)**60)
-
+final_income = d.Decimal(initial_deposit*(1+interest_rate/(100*12))**deposit_term)
 print(final_income, 'BYN')
