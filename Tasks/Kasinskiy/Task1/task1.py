04ab3faa145908c607a1d200648f406f1653c56a
masterkas <mastervolkas@gmail.com>
2022-01-17 20:26:23 +0300

    ver2

--- Tasks/Kasinskiy/Task1/task1.py
+++ Tasks/Kasinskiy/Task1/task1.py
@@ -1,8 +1,9 @@
-import decimal as d
+from decimal import Decimal as dD
 
 initial_deposit = 20000
 interest_rate = 15
 deposit_term = 60
 
-final_income = d.Decimal(initial_deposit*(1+interest_rate/(100*12))**deposit_term)
+final_income = dD(initial_deposit*(1+interest_rate/(100*12))**deposit_term)
 print(final_income, 'BYN')
+print(type(final_income))
