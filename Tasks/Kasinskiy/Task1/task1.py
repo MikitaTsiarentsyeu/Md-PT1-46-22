438364a37f0d920a633d40cb55f1a7b479ab31eb
masterkas <mastervolkas@gmail.com>
2022-01-17 20:39:26 +0300

    Update task1.py

--- Tasks/Kasinskiy/Task1/task1.py
+++ Tasks/Kasinskiy/Task1/task1.py
@@ -4,6 +4,6 @@ initial_deposit = 20000
 interest_rate = 15
 deposit_term = 60
 
-final_income = dD(initial_deposit*(1+interest_rate/(100*12))**deposit_term)
-print(final_income, 'BYN')
-print(type(final_income))
+final_income = dD(dD(initial_deposit)*(1+dD(interest_rate)/(100*12))**dD(deposit_term))
+print(final_income.quantize(dD("1.0000")), 'BYN')
+
