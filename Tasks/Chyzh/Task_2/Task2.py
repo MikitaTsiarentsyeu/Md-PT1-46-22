029c2c4583dbfb941589ca497c53eaa544584049
Oneyouhate <oneyouhate@yandex.ru>
2022-01-19 16:51:34 +0300

    Update Task2.py

--- Tasks/Chyzh/Task_2/Task2.py
+++ Tasks/Chyzh/Task_2/Task2.py
@@ -4,9 +4,92 @@ CT = list(curTime)[0:8]                 # turn string to list splitting millisec
 print("".join(CT))                      # output of current time as a string
 
 usersCurTime = input("Enter current time in hh:mm format:")     #input of current time
-timeDict = {"00":"двенадцать часов", "01":"час", "02":"два часа", "03":"три часа", "04":"четыре часа", "05":"пять часов",
-                "06":"шесть часов", "07":"семь часов", "08":"восемь часов", "09":"девять часов", "10":"десять часов", 
-                "11":"одиннадцать часов", "12":"двенадцать часов", "13":"час", "14":"два часа", "15":"три часа", "16":"четыре часа",
-                "17":"пять часов", "18":"шесть часов", "19":"семь часов", "20":"восемь часов", "21":"девять часов", "22":"десять часов",
-                "23":"одиннадцать часов"}   # define dictionary with time mapping
-print(usersCurTime)
+UCT = list(usersCurTime)
+print(UCT)
+hours = int(UCT[0] + UCT[1])
+minutes = int(UCT[3] + UCT[4])
+x = hours
+y = minutes
+# hours 1 case (<30), 2 case (half), 3 case (> 30 & <45) - case123list
+# hours 4 case (>=45 - till), 5 case (00 minutes) - case45list
+# minutes tillmin (>=45)
+case123list = ["первого", "второго", "третьего", "четвертого", "пятого", "шестого", "седьмого", "восьмого",
+               "девятого", "десятого", "одиннадцатого", "двенадцатого"]
+case45list = ["час", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять",
+          "одиннадцать", "двенадцать"]
+tillmin = ["пятнадцати", "четырнадцати", "тринадцати", "двенадцати", "одиннадцати", "десяти", "девяти", "восьми", 
+           "семи", "шести", "пяти", "четырех", "трех", "двух", "одной"]
+minutes = ["одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", 
+           "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", 
+           "девятнадцать", "двадцать", "двадцать одна", "двадцать две", "двадцать три", "двадцать четыре", "двадцать пять", 
+           "двадцать шесть", "двадцать семь", "двадцать восемь", "двадцать девять"]
+minutes1 = ["тридцать одна", "тридцать две", "тридцать три", "тридцать четыре", "тридцать пять", "тридцать шесть", "тридцать семь", 
+            "тридцать восемь", "тридцать девять", "сорок", "сорок одна", "сорок две", "сорок три", "сорок четыре"]
+
+if y >= 60 or x >= 24:
+    print("Неверно введено время!")
+elif y == 0:
+    if x == 1 or x == 13:
+        print("час")
+    elif x == 0 or (x >= 5 and x <= 12):
+        print(case45list[x-1], "часов")
+    elif x >=2 and x <=4:
+        print(case45list[x-1], "часа")
+    elif x >= 14 and x <=16:
+        print(case45list[x-13], "часа")
+    elif x >= 17 and x <= 23:
+        print(case45list[x-13], "часов")
+elif y == 30:
+    if x <= 12:
+        print("Половина", case123list[x])
+    elif x >=13 and x <= 23:
+        print("Половина", case123list[x-12])
+elif y >=45:
+    if x == 1 or x == 13:
+        if y == 59:
+            print("без", tillmin[y-60], "минуты","час")
+        else:
+            print("без", tillmin[y-60], "минут","час")
+    elif x >=0 and x <=12:
+        if y == 59:
+            print("без", tillmin[y-60], "минуты", case45list[x])
+        else:
+            print("без", tillmin[y-60], "минут", case45list[x])
+    elif x >12 and x <=23:
+        if y == 59:
+            print("без", tillmin[y-60], "минуты", case45list[x-12])
+        else:
+            print("без", tillmin[y-60], "минут", case45list[x-12])
+elif y > 0 and y < 30:
+    if y == 1 or y == 21:
+        if x >=0 and x <=12:
+            print(minutes[y-1], "минута", case123list[x])
+        elif x >12 and x <=23:
+            print(minutes[y-1], "минута", case123list[x-12])
+    elif (y >=2 and y <= 4) or (y >= 22 and y <= 24):
+        if x >=0 and x <=12:
+            print(minutes[y-1], "минуты", case123list[x])
+        elif x >12 and x <=23:
+            print(minutes[y-1], "минуты", case123list[x-12])
+    elif (y >= 5 and y <=10) or (y >= 11 and y <=16) or (y >= 17 and y <=29):
+        if x >=0 and x <=12:
+            print(minutes[y-1], "минут", case123list[x])
+        elif x >12 and x <=23:
+            print(minutes[y-1], "минут", case123list[x-12])
+            
+elif y > 30 and y < 45:
+    if y == 31 or y == 41:
+        if x >=0 and x <=12:
+            print(minutes1[y-31], "минута", case123list[x])
+        elif x >12 and x <=23:
+            print(minutes1[y-31], "минута", case123list[x-12])
+    elif (y >=32 and y <= 34) or (y >= 42 and y <= 44):
+        if x >=0 and x <=12:
+            print(minutes1[y-31], "минуты", case123list[x])
+        elif x >12 and x <=23:
+            print(minutes1[y-31], "минуты", case123list[x-12])
+    elif y >= 35 and y <=40:
+        if x >=0 and x <=12:
+            print(minutes1[y-31], "минут", case123list[x])
+        elif x >12 and x <=23:
+            print(minutes1[y-31], "минут", case123list[x-12])
