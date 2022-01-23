6055d40a5426932602dfb730f598cd0a01e22739
masterkas <mastervolkas@gmail.com>
2022-01-23 10:31:08 +0300

    Update task2.py

--- Tasks/Kasinskiy/Task2/task2.py
+++ Tasks/Kasinskiy/Task2/task2.py
@@ -1,9 +1,5 @@
-from decimal import Decimal as dD
+import datetime
+import time
 
-initial_deposit = 20000
-interest_rate = 15
-deposit_term = 60
-
-final_income = dD(dD(initial_deposit)*(1+dD(interest_rate)/(100*12))**dD(deposit_term))
-print(final_income.quantize(dD("1.0000")), 'BYN')
+print(datetime.time.strftime(time.time()))
 
