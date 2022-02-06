5473574624b4e7890f7698a33a1008b86e6656ec
MaslouSirhei <lderona@gmail.com>
2022-02-06 14:00:26 +0300

    Update task3.py

--- Tasks/Maslov/task3/task3.py
+++ Tasks/Maslov/task3/task3.py
@@ -1,9 +1,12 @@
 from collections import Counter
-a = Counter(input())
-print(a)
 
-
-
+a = Counter(input('Введите слова : '))
+b = int(input('Введите сколько хотите резултатов видеть : '))
+c = Counter(a).most_common(b)
+if len(c) < b:
+    print(f'Ограниченное количество')
+for d in c:
+    print(f'{d[0]} повторяется {d[1]} раз(раза)')
 
 
 from collections import Counter
