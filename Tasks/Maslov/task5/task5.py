c0af88f2025b7a66ea95837a0172277f06225271
MaslouSirhei <lderona@gmail.com>
2022-02-08 22:47:12 +0300

    Update task5.py

--- Tasks/Maslov/task5/task5.py
+++ Tasks/Maslov/task5/task5.py
@@ -1,13 +1,14 @@
-def check_str():pass
-bol=0
-mal=0
-for i in a:
-    if i.isupper():
-            bol+=1
-    if i.islower():
-        mal+=1
-    
+def check_str(a):
+    bol = 0
+    mal = 0
+    for i in a:
+        if i.isupper():
+            bol += 1
+        elif i.islower():
+            mal += 1
+    return bol, mal
+   
 def is_prime(): pass
 def get_ranges():pass
-a='The quick Brow Fox'
-print(check_str)
+a = ('The quick Brow Fox')
+print(check_str(a))
