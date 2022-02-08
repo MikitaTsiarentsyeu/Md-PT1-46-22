8999c935447493b432b5185a49690fc8f3e5d549
MaslouSirhei <lderona@gmail.com>
2022-02-08 22:17:19 +0300

    Update task5.py

---
+++ Tasks/Maslov/task5/task5.py
@@ -0,0 +1,13 @@
+def check_str():pass
+bol=0
+mal=0
+for i in a:
+    if i.isupper():
+            bol+=1
+    if i.islower():
+        mal+=1
+    
+def is_prime(): pass
+def get_ranges():pass
+a='The quick Brow Fox'
+print(check_str)
