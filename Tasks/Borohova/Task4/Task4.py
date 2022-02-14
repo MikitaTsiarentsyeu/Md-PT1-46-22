793819c491f6a1720f6d89d3060778eb7cc8bfd7
julia.borohova <Juli_kasper@mail.ru>
2022-02-14 22:38:55 +0300

    [Task4] - Added spaces between words.

--- Tasks/Borohova/Task4/Task4.py
+++ Tasks/Borohova/Task4/Task4.py
@@ -20,18 +20,39 @@ with open("text.txt", 'r') as f:
         f1 = l.split()
         for i in f1:
             list.append(i)
-#print(list)
+# print(list)
 
 line = ''
 result = open('result.txt', 'w')
+
 for word in list:
-    if len(line) + 1 + len(word) < h:
+    if len(line) + 1 + len(word) <= h:
         if len(line) != 0:
             line += ' ' + word
         else:
             line += word
     else:
-        # print(line)
+        res = ''
+        a = h - len(line)
+        if a == 0:
+            res = line
+        else:
+            l1 = [x for x in line.split()]
+            w = len(l1)
+            if w == 1:
+                res = l1[0]
+            a += (w - 1)
+            spaces = a // (w - 1)
+            spaces1 = a % (w - 1)
+            res = ''
+            for x in l1:
+                res += x + spaces * ' '
+                if spaces1 > 0:
+                    res += ' '
+                    spaces1 -= 1
+        line = res
         result.writelines(line + '\n')
-        line = ''
+        line = word
+if len(line) > 0:
+    result.writelines(line)
 result.close()
