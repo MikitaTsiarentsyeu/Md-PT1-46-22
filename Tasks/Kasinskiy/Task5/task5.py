3a621d924137a85840a9522e7a92e7bfafb7000e
masterkas <mastervolkas@gmail.com>
2022-02-15 18:16:55 +0300

    Update task5.py

--- Tasks/Kasinskiy/Task5/task5.py
+++ Tasks/Kasinskiy/Task5/task5.py
@@ -1,9 +1,10 @@
 def is_prime(x):
-    for i in range(2, int(x**0.5)+1):
-        if x%i == 0:
-            return False
-    return True
-print(is_prime(777))
+    if isinstance(x, int) and x > 1:
+        for i in range(2, int(x ** 0.5) + 1):
+            if x % i == 0:
+                return False
+        return True
+
 
 def check_str(s: str):
     up_count = 0
@@ -13,22 +14,26 @@ def check_str(s: str):
             up_count += 1
         if i.islower():
             low_count += 1
-    print(up_count, low_count)
-check_str("Допуски среднего диаметра    внутреВыыыннВыей резьбы, предназначенной для соединения с наружной конической")
+    print(f'{up_count} - upper case, {low_count} - lower case')
 
 
-def get_ranges(A: list):
-    B = []
-    start = A[0]
+def get_ranges(user_list: list):
+    b = []
+    start = user_list[0]
     j = 0
-    for i in range(1, len(A)):
-        if A[i] - A[i-1] != 1:
-            stop = A[i-1]
-            B.append(f'{start}-{stop}') if ((i) - j) != 1 else B.append(f'{start}')
-            start = A[i]
+    for i in range(1, len(user_list)):
+        if user_list[i] - user_list[i - 1] != 1:
+            stop = user_list[i - 1]
+            b.append(f'{start}-{stop}') if (i - j) != 1 else b.append(f'{start}')
+            start = user_list[i]
             j = i
-    stop = A[-1]
-    B.append(f'{start}-{stop}') if j < (len(A) - 1) else B.append(f'{stop}')
-    print(', '.join(B))
-get_ranges([0,1,2,3,5,6,7,9,11,25,26])
+    stop = user_list[-1]
+    b.append(f'{start}-{stop}') if j < (len(user_list) - 1) else b.append(f'{stop}')
+    print(', '.join(b))
+
+
+
+check_str("The Python Software Foundation (PSF) is a 501 non-profit corporation")
+print(is_prime(707))
+get_ranges([-5, 0, 1, 2, 3, 5, 6, 7, 9, 11, 25, 26])
 
