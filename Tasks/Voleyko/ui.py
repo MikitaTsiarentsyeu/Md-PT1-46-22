351612b29a23650a35a10159a3c17b4e55c86fa0
VoleykoN <nadiavoleyko@gmail.com>
2022-04-03 01:34:20 +0300

    reworked ui

--- Tasks/Voleyko/ui.py
+++ Tasks/Voleyko/ui.py
@@ -2,19 +2,19 @@ import bl
 
 def show_menu():
     menu = bl.menu()
-    return menu
+    print(menu)
 def search(id):
-    return bl.search(id)
+    print(bl.search(id))
 def clean():
-    return bl.clean()
+    print(bl.clean())
 def len_basket():
-    return bl.len_basket()
+    print(bl.len_basket())
 def add_items(id,quantity):
-    return bl.add_items(id,quantity)
+    print( bl.add_items(id,quantity))
 def remove_item(id):
-    return bl.remove_items(id)
+    print(bl.remove_items(id))
 def receipt():
-    return bl.get_receipt()
+    print(bl.get_receipt())
 
 
 def main_flow():
@@ -34,23 +34,24 @@ def main_flow():
         
         """)
         if chosed_action == '1':
-            print(show_menu())
+            show_menu()
         elif chosed_action == '2':
             id = int(input('Please,enter id of the item: '))
-            print(search(id))            
+            search(id)            
         elif chosed_action == '3':
             id = int(input('Please,enter id of the item: '))
             quantity = int(input('Please,enter quantity of items: '))
-            print(add_items(id,quantity))            
+            add_items(id,quantity)        
         elif chosed_action == '4':
             id = int(input('Please,enter id of the item: '))
-            print(remove_item(id))           
+            remove_item(id)          
         elif chosed_action == '5':
-            print(len_basket())            
+            len_basket()            
         elif chosed_action == '6':
-            print(clean())          
+            clean()         
         elif chosed_action == '7':
-            print(receipt())   
+            receipt() 
         elif chosed_action == '8':
             print('Goodbye! Have a good day!')
             break 
+
