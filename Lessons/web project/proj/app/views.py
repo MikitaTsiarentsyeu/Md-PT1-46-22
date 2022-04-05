0038c835be05cb96b941a3b871e872d58e80c18a
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2022-04-06 00:32:40 +0600

    add lesson 05/04

--- Lessons/web project/proj/app/views.py
+++ Lessons/web project/proj/app/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Author, Post
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+# def posts(request):
+#     post_list = ""
+#     for p in Post.objects.all():
+#         post_list += f"<li><h3>{p.title}</h3><p>by {p.author.name}</p></li>"
+
+#     res = f"<h1>All posts:</h1><ul>{post_list}</ul>"
+
+#     return HttpResponse(res)
+
+def posts(request):
+    all_posts = Post.objects.all()
+    return render(request, 'posts.html', {'posts':all_posts})
+
+# def post(request, id):
+#     p = Post.objects.get(id=id)
+#     res = f"<h3>{p.title}</h3><p>by {p.author.name}</p>"
+#     return HttpResponse(res)
+
+def post(request, id):
+    p = Post.objects.get(id=id)
+    return render(request, 'post.html', {'post':p})
