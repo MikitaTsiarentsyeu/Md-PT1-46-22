6cf74fb007d7c5cbd6b9a21a1d52ba88c0a1dffd
MaslouSirhei <lderona@gmail.com>
2022-04-24 16:22:59 +0300

    update

--- Tasks/Maslov/din/mysite/prodaja/urls.py
+++ Tasks/Maslov/din/mysite/prodaja/urls.py
@@ -2,12 +2,19 @@ from re import template
 from django.conf.urls import url, include
 from django.views.generic import ListView,DetailView
 from prodaja.models import Articles
-from . import views
+from . import views 
+
 
 
 urlpatterns = [
     url(r'^price/$', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20],
-    template_name="prodaja/price.html")), 
-    url(r'^login/$', views.user_login, name='login'),
+ template_name="prodaja/price.html")), 
+    # url(r'^price/$', views.price, name='price'),
+   
+    url(r'^register/$', views.register, name='register'),
+    url(r'^register_done/$', views.register_done, name='register_done'),
+    
     
 ]
+
+   
--- Tasks/Maslov/din/mysite/prodaja/views.py
+++ Tasks/Maslov/din/mysite/prodaja/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+
+from .forms import  UserRegistrationForm
 from .models import Product
-from .forms import LoginForm
-
-
+from django.shortcuts import redirect, render
 
 
 
@@ -21,25 +18,20 @@ def all_product(request):
   }
 
   return render(request, 'prodaja/price.html', context)
-
-# def login(request):
-#     return render(request, 'prodaja/login.html')
-
-def user_login(request):
+def register_done(request):
+    return render(request, 'prodaja/register_done.html')
+ 
+def register(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully') 
-                     
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+           
+            new_user = user_form.save(commit=False)
+        
+            new_user.set_password(user_form.cleaned_data['password'])
+            
+            new_user.save()
+            return render(request, 'prodaja/register_done.html', {'new_user': new_user})
     else:
-        form = LoginForm()
-    return render(request, 'prodaja/login.html', {'form': form})
+        user_form = UserRegistrationForm()
+    return render(request, 'prodaja/register.html', {'user_form': user_form})
